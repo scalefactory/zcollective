a52c38407dddd6c05736ef8588e3fc9024c497cb
Jon Topper <jon@scalefactory.com>
2012-11-20 18:17:02 +0000

    Improved commenting

--- zcollective.rb
+++ zcollective.rb
@@ -105,7 +105,11 @@ zabbix_client.request( 'host.get',
 
     # Iterate through hostinterfaces, looking for zabbix agent type
     #  interfaces.
-
+    #
+    #  I'm not sure how we should handle multiple interfaces here
+    #   but it seems a safe assumption that there will only be one
+    #   agent type interface per machine.
+    
     zabbix_client.request( 'hostinterface.get',
         'hostids' => host['hostid'], 
         'output'  => 'extend'
@@ -147,6 +151,9 @@ mc.progress = false
 
 mc.discover.sort.each do |host|
 
+    # MCollective returns FQDN name, and we probably want to use the short
+    #  form name in zabbix.
+    
     short_hostname = host.split('.').first
 
     log.info("Host: #{short_hostname} (#{host})")
@@ -156,8 +163,11 @@ mc.discover.sort.each do |host|
         { "identity" => host }
     ).first
 
+    # XXX using the ipaddress fact is probably the wrong thing to do here
+    # We should take an 'admin interface range' CIDR from the commandline
+    #  and use the address matching that instead.
+    
     log.info("\tIP #{inventory[:data][:facts]['ipaddress']}")
-
 
     hosts[ short_hostname ][:mcollective][:ip]      = inventory[:data][:facts]['ipaddress']
     hosts[ short_hostname ][:mcollective][:classes] = inventory[:data][:classes]
@@ -171,9 +181,22 @@ mc.disconnect
 
 hosts.each do |host,data|
 
+    ###### Condition 1 #############################################
+    #
+    # Hosts that are found by mcollective but that aren't in zabbix
+    #  should be added.
+    
     if data.has_key?(:mcollective) and !data.has_key?(:zabbix)
 
         log.info( "Host #{host} found by mcollective but not in zabbix" )
+
+        # If mcollective finds a host, but zabbix doesn't list one by
+        #  that name, we're going to add it.
+
+        # Iterate through the classes reported by mcollective for this
+        #  host. If the class name matches the name of a zabbix template,
+        #  get the ID and add an object to the templates_to_add array.
+        #  This will be passed to the zabbix API call.
 
         templates_to_add = []
         data[:mcollective][:classes].each do |template|
@@ -184,6 +207,11 @@ hosts.each do |host,data|
         end
 
         unless options[:noop]
+
+            # If we're not in --noop mode, create the host with the
+            #  zabbix API.  Hosts need at least one interface (for now
+            #  we're just adding a Zabbix agent interface), and need
+            #  to be in a group.
 
             resp = zabbix_client.request( 'host.create',
                 'host'       => host,
@@ -203,6 +231,8 @@ hosts.each do |host,data|
                 'templates' => templates_to_add
             )
 
+            # This call returns the created host id
+
             new_hostid = resp['hostids'].first
 
             log.info("Host #{host} added as ID #{new_hostid}")
@@ -211,27 +241,48 @@ hosts.each do |host,data|
 
     end
 
+    ###### Condition 2 #############################################
+    # If zabbix contains a host that mcollective knows nothing about
+    #  we leave it alone but report it.
+
     if data.has_key?(:zabbix) and !data.has_key?(:mcollective)
 
         log.warn( "Host #{host} found in zabbix but not by mcollective" )
 
     end
 
+    ###### Condition 3 #############################################
+    # Hosts in zabbix and mcollective are checked to ensure that 
+    #  they are linked with at least the templates they should be
+    
     if data.has_key?(:zabbix) and data.has_key?(:mcollective)
 
         log.info( "Host #{host} in both zabbix and mcollective" )
 
         templates_to_add = []
 
+        # Iterate through the classes mcollective lists for the host
+
         data[:mcollective][:classes].each do |template|
 
+            # Ignore any that don't match the name of a zabbix template
             next unless zabbix_templates.has_key?( template )
 
             log.debug("\tHas mcollective class #{template} matching a zabbix template")
 
             if data[:zabbix][:templates].index( template )
-                log.debug("\tZabbix host already has a template for this class")
+
+                # The host in zabbix is already linked to a template with the name
+                #  of this class.  We do nothing here.
+                #
+                log.debug("\tZabbix host already linked to a template for this class")
+
             else
+
+                # Zabbix shows that although it knows about a template with this class
+                #  name, the host in question isn't linked to it.  We add this
+                #  template to a list of those that are missing in zabbix.
+
                 log.info("\tZabbix #{host} does not have a template for #{template}")
                 templates_to_add.push( { 'templateid' => zabbix_templates[ template ] } )
             end
@@ -239,6 +290,9 @@ hosts.each do |host,data|
         end
 
         if !options[:noop] and templates_to_add.count > 0
+
+            # If we're not running --noop and we found missing templates,
+            #  link the zabbix host with these.
 
             zabbix_client.request( 'template.massadd',
                 'templates' => templates_to_add,
